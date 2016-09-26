9addd0b8989b8bb0e73589ec779c30526669388c
Prastowo aGung Widodo <agung@transformatika.com>
2016-09-26 11:55:10 +0700

    fix css

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
 window.$ = window.jQuery = require('jquery');
 
-loadStyleSheet('../jspm_packages/uspace-sdk-ui@0.1.0/libs/libs-css/default.css');
-loadStyleSheet('../jspm_packages/uspace-sdk-ui@0.1.0/libs/libs-css/ionicons/ionicons.min.css');
-loadStyleSheet('../jspm_packages/uspace-sdk-ui@0.1.0/libs/libs-css/default.css');
-loadStyleSheet('../jspm_packages/uspace-sdk-ui@0.1.0/libs/libs-owlcarousel/owl.carousel.css');
-loadStyleSheet('../jspm_packages/uspace-sdk-ui@0.1.0/libs/libs-owlcarousel/owl.theme.css');
-loadStyleSheet('../jspm_packages/uspace-sdk-ui@0.1.0/libs/libs-owlcarousel/owl.transitions.css');
+loadStyleSheet('../jspm_packages/uspace-sdk-ui@0.1.0-2/libs/libs-css/default.css');
+loadStyleSheet('../jspm_packages/uspace-sdk-ui@0.1.0-2/libs/libs-css/ionicons/ionicons.min.css');
+loadStyleSheet('../jspm_packages/uspace-sdk-ui@0.1.0-2/libs/libs-css/default.css');
+loadStyleSheet('../jspm_packages/uspace-sdk-ui@0.1.0-2/libs/libs-owlcarousel/owl.carousel.css');
+loadStyleSheet('../jspm_packages/uspace-sdk-ui@0.1.0-2/libs/libs-owlcarousel/owl.theme.css');
+loadStyleSheet('../jspm_packages/uspace-sdk-ui@0.1.0-2/libs/libs-owlcarousel/owl.transitions.css');
 
 loadSemanticScript();
 
@@ -19,8 +19,8 @@ function loadStyleSheet(path)
     document.head.appendChild(styleSheetElement);
 }
 
-require('libs/libs-css/semantic.min.js');
-require('libs/libs-owlcarouse/owl.carouse.min.js');
+require('./libs/libs-css/semantic.min.js');
+require('./libs/libs-owlcarouse/owl.carouse.min.js');
 
 module.exports = {
     DropZone: require('./src/Components/DropZone'),
