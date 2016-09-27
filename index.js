0fad0a400695deb6d5a6e98c91caf867af52503e
Prastowo aGung Widodo <agung@transformatika.com>
2016-09-27 07:15:29 +0700

    fix pikaday stylesheet

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ import './libs/css/ionicons/ionicons.min.css!';
 import './libs/owlcarousel/owl.carousel.css!';
 import './libs/owlcarousel/owl.theme.css!';
 import './libs/owlcarousel/owl.transitions.css!';
-import './libs/pikaday/css/pikaday.css!';
-import './libs/pikaday/css/theme.css!';
+import './libs/css/pikaday.css!';
+import './libs/css/theme.css!';
 
 import {$, jQuery} from 'jquery';
 import './libs/css/semantic.min.js';
 import './libs/owlcarousel/owl.carousel.min.js';
 import './libs/nicescroll/nicescroll.min.js';
 import './libs/ckeditor/ckeditor.js';
-import './libs/pikaday/pikaday.js';
